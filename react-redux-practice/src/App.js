6067e5a57566cae75aa8c2bfc2fd7205b48a6e82
github username <github email address>
2020-01-14 17:32:01 +0100

    paintings now in state

--- react-redux-practice/src/App.js
+++ react-redux-practice/src/App.js
@@ -3,11 +3,14 @@ import './App.css'
 import data from './data'
 
 class App extends React.Component {
-  // state = {
-  //   paintings: [],
-  //   indeciesToPlay: [],
-  //   showStartGameButton: true
-  // }
+  state = {
+    data: []
+  }
+
+  componentDidMount () {
+    this.setState({ data })
+    // imports paintings from data file
+  }
 
   render () {
     return <div>Does this work?</div>
