cf3f93206a1ac43775f7a3228018111b74e2f416
github username <github email address>
2020-01-14 17:25:46 +0100

    initialized react app part 2

---
+++ react-redux-practice/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import data from './data'
+
+class App extends React.Component {
+  // state = {
+  //   paintings: [],
+  //   indeciesToPlay: [],
+  //   showStartGameButton: true
+  // }
+
+  render () {
+    return <div>Does this work?</div>
+  }
+}
+
+export default App
